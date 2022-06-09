299724a9c23429fda28b418ceb29b20fd27b393c
yunhyeon <won@a.a>
2022-06-09 21:50:58 +0900

    0609

---
+++ src/com/jo4/event/Event.java
@@ -0,0 +1,7 @@
+package com.jo4.event;
+
+public class Event {
+ public void event1() {
+	 System.out.println("이달의 이벤트 보기를 선택!");
+ }
+}
---
+++ src/com/jo4/event/Event1.java
@@ -0,0 +1,7 @@
+package com.jo4.event;
+
+public class Event1 {
+public void event2(){
+	System.out.println("전체 이벤트 보기를 선택!");
+}
+}
