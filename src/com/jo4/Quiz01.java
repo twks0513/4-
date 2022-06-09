09e0e0b0cda225a965844524997ee0f1e55af59c
twks0513 <twks0513@gmail.com>
2022-06-09 20:48:58 +0900

    최초틀생성

---
+++ src/com/jo4/Quiz01.java
@@ -0,0 +1,33 @@
+package com.jo4;
+
+import java.util.Scanner;
+
+public class Quiz01 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("1.회원관련 2.이벤트관련");
+		int choice = scan.nextInt();
+		if(choice == 1) {
+			System.out.println("회원관련기능입니다");
+			System.out.println("1.회원저장기능 2.회원 수 보기");
+			int memchoice = scan.nextInt();
+			if(memchoice == 1) {
+				System.out.println("회원저장기능");
+			}else if(memchoice ==2) {
+				System.out.println("회원 수 보는 기능");
+			}
+		}else if(choice ==2) {
+			System.out.println("이벤트관련기능");
+			System.out.println("1.이달의 이벤트 2. 전체 이벤트");
+			int eventchoice = scan.nextInt();
+			if(eventchoice ==1) {
+				System.out.println("이달의 이벤트기능");
+			}else if (eventchoice ==2) {
+				System.out.println("전체 이벤트 기능");
+			}
+		}
+			
+
+	}
+
+}
