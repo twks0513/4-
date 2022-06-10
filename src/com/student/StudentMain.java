d83046b94c16203bd0476fd301e6403cbbc26efa
yunhyeon <won@a.a>
2022-06-10 19:18:39 +0900

    0610 김윤현 학생관리틀

--- src/com/student/StudentMain.java
+++ src/com/student/StudentMain.java
@@ -2,11 +2,12 @@ package com.student;
 import java.util.*;
 //import com.student.BEH_Student;
 //import com.student.KJH_Student; //Á÷Á¢ ÇÏ½Ç¶© º»ÀÎ°Å ÁÖ¼®ÇØÁ¦ÇØ¼­ »ç¿ëÇØÁÖ¼¼¿ä
-//import com.student.KYH_Student;
+import com.student.KYH_Student;
 
 public class StudentMain {
 
 	public static void main(String[] args) {
+		KYH_Student yh = new KYH_Student();
 		Scanner scan = new Scanner(System.in);
 		int go;
 		System.out.println("1.¹æÀºÇõ 2.±İÁöÇı 3.±èÀ±Çö");
@@ -19,7 +20,7 @@ public class StudentMain {
 			System.out.println("±İÁöÇı");
 			break;
 		case 3: 
-			System.out.println("±èÀ±Çö");					
+			yh.start();					
 			break;
 		}
 
